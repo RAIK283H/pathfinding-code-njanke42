4a769bde10799d481e5aa4cb79ab585dc2b3cff0
njanke42 <noahdjanke42@gmail.com>
2024-10-21 21:53:40 -0500

    Algorithms Finished

--- config_data.py
+++ config_data.py
@@ -5,10 +5,10 @@ starting_player_index = 0
 starting_graph_index = 0
 
 player_data = [
-    ["Test", "1.png", colors.YELLOW],
-    ["Random", "2.png", colors.ORANGE],
-#    ["DFS", "3.png", colors.PURPLE],
-#    ["BFS", "4.png", colors.BLUE],
+#    ["Test", "1.png", colors.YELLOW],
+#    ["Random", "2.png", colors.ORANGE],
+    ["DFS", "3.png", colors.PURPLE],
+    ["BFS", "4.png", colors.BLUE],
 #    ["Dijkstra", "5.png", colors.GREEN]
 ]
 
--- pathing.py
+++ pathing.py
@@ -4,11 +4,11 @@ import random
 
 def set_current_graph_paths():
     global_game_data.graph_paths.clear()
-    global_game_data.graph_paths.append(get_test_path())
-    global_game_data.graph_paths.append(get_random_path())
+    #global_game_data.graph_paths.append(get_test_path())
+    #global_game_data.graph_paths.append(get_random_path())
     global_game_data.graph_paths.append(get_dfs_path())
     global_game_data.graph_paths.append(get_bfs_path())
-    global_game_data.graph_paths.append(get_dijkstra_path())
+    #global_game_data.graph_paths.append(get_dijkstra_path())
 
 
 def get_test_path():
@@ -39,15 +39,115 @@ def get_random_path():
         nextNode = possibilities[random.randint(0,len(possibilities) - 1)]
         path.append(nextNode)
         currentIndex = nextNode
+    assert path is not None
+    assert path[len(path) - 1] == len(graphData) - 1
     return path
 
 
 def get_dfs_path():
-    return [1,2]
+    target_node = global_game_data.target_node[global_game_data.current_graph_index]
+    path = dfs_path_creation(0, target_node)
+    graphData = graph_data.graph_data[global_game_data.current_graph_index]
+    secondPath = dfs_path_creation(target_node, len(graphData) - 1)
+    return path + secondPath
 
+def dfs_path_creation(start, end):
+    graphData = graph_data.graph_data[global_game_data.current_graph_index]
+    assert graphData is not None
+
+    frontier = []
+    frontier.append(start)
+
+    visited = []
+    visited.append(start)
+
+    parents = {}
+
+    while frontier:
+        #Get the next element in the queue
+        currentIndex = frontier.pop()
+        #Mark the next element as visited
+        if currentIndex not in visited:
+            visited.append(currentIndex)
+
+        #Get neighbors of the current cell (passageways)
+        neighbors = graphData[currentIndex][1]
+
+        #Add neighbors to the queue
+        for i in range(len(neighbors)):
+            neighbor = neighbors[i]
+
+            # see if we've visited this cell
+            if neighbor not in visited:
+                visited.append(neighbor)
+                # add current as the neighbor's parent
+                parents[neighbor] = currentIndex
+                # add the neighbor to the queue
+                frontier.append(neighbor)
+        #Target found?
+        if (currentIndex == end):
+            break
+
+    #retrace path
+    path = []
+    currentIndex = end
+    while currentIndex != start:
+        path.insert(0, currentIndex)
+        currentIndex = parents[currentIndex]
+    return path
 
 def get_bfs_path():
-    return [1,2]
+    target_node = global_game_data.target_node[global_game_data.current_graph_index]
+    path = bfs_path_creation(0, target_node)
+    graphData = graph_data.graph_data[global_game_data.current_graph_index]
+    secondPath = bfs_path_creation(target_node, len(graphData) - 1)
+    return path + secondPath
+
+def bfs_path_creation(start, end):
+    graphData = graph_data.graph_data[global_game_data.current_graph_index]
+    assert graphData is not None
+
+    frontier = []
+    frontier.append(start)
+
+    visited = []
+    visited.append(start)
+
+    parents = {}
+
+    while frontier:
+        #Get the next element in the queue
+        currentIndex = frontier.pop(0)
+        #Mark the next element as visited
+        if currentIndex not in visited:
+            visited.append(currentIndex)
+
+        #Get neighbors of the current cell (passageways)
+        neighbors = graphData[currentIndex][1]
+
+        #Add neighbors to the queue
+        for i in range(len(neighbors)):
+            neighbor = neighbors[i]
+
+            # see if we've visited this cell
+            if neighbor not in visited:
+                visited.append(neighbor)
+                # add current as the neighbor's parent
+                parents[neighbor] = currentIndex
+                # add the neighbor to the queue
+                frontier.append(neighbor)
+        #Target found?
+        if (currentIndex == end):
+            break
+
+    #retrace path
+    path = []
+    currentIndex = end
+    while currentIndex != start:
+        path.insert(0, currentIndex)
+        currentIndex = parents[currentIndex]
+    return path
+
 
 
 def get_dijkstra_path():
