8effdb8e2d7dcb5ef88eb813a8ad95c419ed9c9a
njanke42 <noahdjanke42@gmail.com>
2024-11-18 13:05:28 -0600

    Algorithm Finished

--- config_data.py
+++ config_data.py
@@ -5,11 +5,11 @@ starting_player_index = 0
 starting_graph_index = 0
 
 player_data = [
-    ["Test", "1.png", colors.YELLOW],
+    #["Test", "1.png", colors.YELLOW],
     #["Random", "2.png", colors.ORANGE],
-    ["DFS", "3.png", colors.PURPLE],
-    ["BFS", "4.png", colors.BLUE],
-#    ["Dijkstra", "5.png", colors.GREEN]
+    #["DFS", "3.png", colors.PURPLE],
+    #["BFS", "4.png", colors.BLUE],
+    ["Dijkstra", "5.png", colors.GREEN]
 ]
 
 display_size_right = 400
--- pathing.py
+++ pathing.py
@@ -1,14 +1,16 @@
 import graph_data
 import global_game_data
 import random
+import heapq as heap
+import math
 
 def set_current_graph_paths():
     global_game_data.graph_paths.clear()
-    global_game_data.graph_paths.append(get_test_path())
+    #global_game_data.graph_paths.append(get_test_path())
     #global_game_data.graph_paths.append(get_random_path())
-    global_game_data.graph_paths.append(get_dfs_path())
-    global_game_data.graph_paths.append(get_bfs_path())
-    #global_game_data.graph_paths.append(get_dijkstra_path())
+    #global_game_data.graph_paths.append(get_dfs_path())
+    #global_game_data.graph_paths.append(get_bfs_path())
+    global_game_data.graph_paths.append(get_dijkstra_path())
 
 
 def get_test_path():
@@ -176,4 +178,54 @@ def bfs_path_creation(start, end):
 
 
 def get_dijkstra_path():
-    return [1,2]
+    graphData = graph_data.graph_data[global_game_data.current_graph_index]
+    target_node = global_game_data.target_node[global_game_data.current_graph_index]
+
+    path = dijkstras_between_two_nodes(graphData, 0, target_node)
+
+    path_2 = dijkstras_between_two_nodes(graphData, target_node, len(graphData) - 1)
+    path_2.remove(target_node)
+    path += path_2
+
+    return path
+
+def dijkstras_between_two_nodes(graph, start_index, end_index):
+    frontier = []
+    heap.heappush(frontier, (0, start_index, None))
+    parents = {}
+    visited = set()
+    for i in range(0, len(graph)):
+        heap.heappush(frontier, (math.inf, i, None))
+        parents[i] = (math.inf, None)
+    parents[0] = (0, None) #Distance, parent
+
+
+    while frontier:
+        current_distance, current_node, parent_node = heap.heappop(frontier)
+
+        #We already popped this node once with a shorter distance, we don't need to do it again
+        if current_node in visited:
+            continue
+        
+        visited.add(current_node)
+        if parents[current_node][0] > current_distance:
+            parents[current_node] = (current_distance, parent_node)
+        
+        neighbors = graph[current_node][1]
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                new_distance = current_distance + math.sqrt(math.pow(graph[current_node][0][0] - graph[neighbor][0][0],2) + math.pow(graph[current_node][0][1] - graph[neighbor][0][1],2))
+                if new_distance < parents[neighbor][0]:
+                    parents[neighbor] = (new_distance, current_node)
+                    heap.heappush(frontier, (new_distance, neighbor, current_node))
+    
+    path = []
+    path.append(end_index)
+    parent = parents[end_index][1]
+    print(parents)
+    while parent is not None:
+        path.append(parent)
+        parent = parents[parent][1]
+    path.reverse()
+    return path
+
